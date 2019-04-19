1f5dd4ce6515b2d50b7ec020fbcd0aaaddace01b
Jordan <jchapman134569@gmail.com>
2019-04-19 18:59:02 -0400

    Initial UI setup and classes

---
+++ src/CutiePy.cpp
@@ -0,0 +1,41 @@
+﻿#include "CutiePy.hpp"
+
+#include <QDockWidget>
+#include <QApplication>
+
+MainWindow::MainWindow() {
+	auto textEdit = new QPlainTextEdit();
+	setCentralWidget(textEdit);
+	auto consoleContainer = new QDockWidget(this);
+	auto console = new PythonConsole();
+	consoleContainer->setWidget(console);
+	addDockWidget(Qt::DockWidgetArea::BottomDockWidgetArea, consoleContainer);
+}
+
+PythonConsole::PythonConsole(QWidget* parent) : QPlainTextEdit(parent) {
+	QPalette p = palette();
+	p.setColor(QPalette::Base, Qt::black);
+	p.setColor(QPalette::Text, Qt::green);
+	setPalette(p);
+	auto consoleFont = QFont("Courier New");
+	consoleFont.setStyleHint(QFont::StyleHint::Monospace);
+	setFont(consoleFont);
+}
+
+int main(int argc, char* argv[])
+{
+	auto guiApp = true;
+	for (int i = 1; i < argc; ++i)
+		if (!qstrcmp(argv[i], "-no-gui"))
+			guiApp = false;
+	if (guiApp) {
+		auto app = std::make_unique<QApplication>(argc, argv);
+		MainWindow main;
+		main.show();
+		return app->exec();
+	}
+	else {
+		// start non-GUI version...
+		return -1;
+	}
+}
---
+++ src/CutiePy.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <QMainWindow>
+#include <QPlainTextEdit>
+
+class MainWindow : public QMainWindow {
+	Q_OBJECT
+
+public:
+	MainWindow();
+};
+
+class PythonConsole: public QPlainTextEdit {
+	Q_OBJECT
+
+public:
+	PythonConsole(QWidget* parent = nullptr);
+
+private:
+	QString currentCommand;
+};
